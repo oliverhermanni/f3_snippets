469225822d3c9a54008d274a1d9242c147194446
oliverhermanni <oliver@hermanni.info>
2014-02-19 23:58:14 +0100

    Moved url check to own function

--- asset_manager/asset_manager.php
+++ asset_manager/asset_manager.php
@@ -8,12 +8,9 @@ $f3->set('css_assets', function() {
     $theme = $f3->get('theme');
 
     $assets = func_get_args();
-
     $res = '';
-    $audit = \Audit::instance();
-
     foreach($assets as $asset) {
-        $audit->url($asset) ? $url = $asset : $url = '/assets/'.$theme.'/'.$asset;
+        validUrl($asset) ? $url = $asset : $url = '/assets/'.$theme.'/'.$asset;
         $res .= '<link href="'.$url.'" rel="stylesheet">';
     }
 
@@ -30,13 +27,14 @@ $f3->set('js_assets', function(){
     $assets = func_get_args();
 
     $res = '';
-    $audit = \Audit::instance();
-
     foreach($assets as $asset) {
-        $audit->url($asset) ? $url = $asset : $url = '/assets/'.$theme.'/'.$asset;
+        validUrl($asset) ? $url = $asset : $url = '/assets/'.$theme.'/'.$asset;
         $res .= '<script src="'.$url.'"></script>';
     }
 
     return $res;
 });
 
+function validUrl ($url) {
+    return \Audit::instance()->url($url);
+}
